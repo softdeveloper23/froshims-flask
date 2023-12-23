a338cb1f232618d23064151651e7eb5276f9c5c7
unknown <brannongarrett77@gmail.com>
2023-12-23 18:18:29 +0100

    Updated code

--- app.py
+++ app.py
@@ -5,3 +5,9 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/register", methods=["POST"])
+def register():
+    if not request.form.get("name") or not request.form.get("sport"):
+        return render_template("failure.html")
+    return render_template("success.html")
